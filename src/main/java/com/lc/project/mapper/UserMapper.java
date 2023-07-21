6cef3071707065826c515ed3b0a4f6f348f25300
罗晨 <1641767724@qq.com>
2023-07-22 00:11:06 +0800

    个人模板

---
+++ src/main/java/com/lc/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.lc.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lc.project.model.entity.User;
+
+/**
+ * @Entity com.Lc.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
